a8769e2308102be0470433b3482c28e205f15d28
antoine <kima0012@gmail.com>
2018-08-12 12:57:52 +0200

    Create new script usable in multiscreen

--- reindeerScript.py
+++ reindeerScript.py
@@ -37,6 +37,12 @@ def left_click():
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 
+def press_f5():
+    win32api.keybd_event(0x74, 0, 0, 0)
+    time.sleep(.05)
+    win32api.keybd_event(0x74, 0, win32con.KEYEVENTF_KEYUP, 0)
+
+
 def mouse_pos(cord):
     win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
 
@@ -45,7 +51,7 @@ def get_cords():
     x, y = win32api.GetCursorPos()
     x = x - x_pad
     y = y - y_pad
-    print x, y
+    return x, y
 
 
 def is_on_known_coord():
@@ -60,7 +66,8 @@ def click_on_coord(coord):
     left_click()
 
 
-def start_script():
+# Only work one time, after one pop reindeer won't spawn if page is not reload i guess
+def old_script():
     # Initialize cursor position
     mouse_pos(reindeer_coord)
     print('\033[93m' + 'Let the hunt begin !')
@@ -80,8 +87,27 @@ def start_script():
     print('\033[93m' + '=== Total number of reindeer : ' + str(i) + ' ===')
 
 
+def better_script():
+    print('\033[93m' + 'Let the hunt begin !')
+    # Script need to be stopped
+    while 1:
+        im = screen_grab()
+        # Check if pixel color is not matching the dealabs default background
+        if im.getpixel(reindeer_coord) != (233, 234, 237):
+            current_user_coord = get_cords()
+            click_on_coord(reindeer_coord)
+            press_f5()
+            print('\033[92m' + '=== Reindeer captured ===')
+            mouse_pos(current_user_coord)
+        time.sleep(2)
+
+
 def main():
-    start_script()
+    print('\033[95m' + 'Put Dealabs in fullscreen on your main screen (1920x1080)')
+    print('\033[95m' + 'Don''t let any other program go in front of your web browser in the lower right 1/4 of your screen')
+    print('\033[95m' + 'Press CTRL+C on this terminal to kill the process')
+    raw_input('\033[91m' + '*** Press any key to start ***')
+    better_script()
 
 
 if __name__ == '__main__':
